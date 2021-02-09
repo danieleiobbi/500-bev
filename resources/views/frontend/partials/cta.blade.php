ff41cfe26a9c58e3cb55a4e3a3290c00649a51d1
daniele.iobbi <daniele.iobbi@gmail.com>
2021-02-09 09:29:00 +0100

    Cambiato i link delle cta

--- resources/views/frontend/partials/cta.blade.php
+++ resources/views/frontend/partials/cta.blade.php
@@ -7,21 +7,21 @@
         </div>
         <div class="row text-center justify-content-center mb-lg-3">
             <div class="col-12 col-lg-6 text-lg-right mb-3 mb-lg-0 gs_reveal gs_reveal_fromLeft">
-                <a class="btn btn-primary btn-cta border"  href="{!! _i("//www.fiat.it/car-configurator/").'?source=brochure' !!}" target="_blank">
+                <a class="btn btn-primary btn-cta border" data-params='{"cta":"Car Configurator"}' href="{!! _i("//www.fiat.it/car-configurator/").'?source=brochure' !!}" target="_blank">
                     <i class="mbri-icon icon-carconfigurator l-icon"></i><span>{!! _i("Car Configurator") !!}</span></a>
             </div>
             <div class="col-12 col-lg-6 text-lg-left  mb-3 mb-lg-0 gs_reveal gs_reveal_fromRight">
-                <a class="btn btn-primary btn-cta border" href="{!! _i("//www.fiat.co.uk/quote-request").'?source=brochure' !!}" target="_blank">
+                <a class="btn btn-primary btn-cta border" data-params='{"cta":"Get a Quote"}' href="{!! _i("//www.fiat.it/preventivo-auto").'?source=brochure' !!}" target="_blank">
                     <i class="mbri-icon icon-quotation l-icon"></i><span>{!! _i("Get a Quote") !!}</span></a>
             </div>
         </div>
         <div class="row text-center justify-content-center">
             <div class="col-12 col-lg-6 text-lg-right mb-3 mb-lg-0 gs_reveal gs_reveal_fromLeft">
-                <a class="btn btn-primary btn-cta border"  href="{!! _i("//www.fiat.co.uk/test-drive").'?source=brochure' !!}" target="_blank">
+                <a class="btn btn-primary btn-cta border" data-params='{"cta":"Test Drive"}' href="{!! _i("//www.fiat.it/test-drive").'?source=brochure' !!}" target="_blank">
                     <i class="mbri-icon icon-test-drive l-icon"></i><span>{!! _i("Test Drive") !!}</span></a>
             </div>
             <div class="col-12 col-lg-6 text-lg-left gs_reveal gs_reveal_fromRight">
-                <a class="btn btn-primary btn-cta border"  href="{!! _i("//www.fiat.com/where-we-are").'?source=brochure' !!}" target="_blank">
+                <a class="btn btn-primary btn-cta border" data-params='{"cta":"Dealer Locator"}' href="{!! _i("//www.fiat.com/where-we-are").'?source=brochure' !!}" target="_blank">
                     <i class="mbri-icon icon-dealerlocator l-icon"></i><span>{!! _i("Dealer Locator") !!}</span></a>
             </div>
         </div>
